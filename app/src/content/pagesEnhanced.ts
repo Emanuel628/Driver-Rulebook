e10adff667564ee77f9679c6d6a09396a20ce9e4
Emanuel628 <144055605+Emanuel628@users.noreply.github.com>
2026-05-20 16:49:53 -0400

    Wire Chapter 9 into enhanced guide content

--- app/src/content/pagesEnhanced.ts
+++ app/src/content/pagesEnhanced.ts
@@ -1,8 +1,9 @@
 import { drawerGroups, guidePages as baseGuidePages } from './pages';
 import { sixtySeventyRecapPage } from './chapter7';
 import { resetRestartPage } from './chapter8';
+import { sleeperBerthPage } from './chapter9';
 
-const researchedPages = [sixtySeventyRecapPage, resetRestartPage];
+const researchedPages = [sixtySeventyRecapPage, resetRestartPage, sleeperBerthPage];
 
 export const guidePages = baseGuidePages.map(page =>
   researchedPages.find(researchedPage => researchedPage.id === page.id) ?? page
