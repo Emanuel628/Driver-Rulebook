af0270a8a70540180f26e18b3e725610efd0e128
Emanuel628 <144055605+Emanuel628@users.noreply.github.com>
2026-05-20 17:00:05 -0400

    Tighten guide content export typing

--- app/src/content/pagesEnhanced.ts
+++ app/src/content/pagesEnhanced.ts
@@ -1,14 +1,15 @@
+import type { GuidePage } from '../types/content';
 import { drawerGroups, guidePages as baseGuidePages } from './pages';
 import { sixtySeventyRecapPage } from './chapter7';
 import { resetRestartPage } from './chapter8';
 import { sleeperBerthPage } from './chapter9';
 
-const researchedPages = [sixtySeventyRecapPage, resetRestartPage, sleeperBerthPage];
+const researchedPages: GuidePage[] = [sixtySeventyRecapPage, resetRestartPage, sleeperBerthPage];
 
-export const guidePages = baseGuidePages.map(page =>
-  researchedPages.find(researchedPage => researchedPage.id === page.id) ?? page
-);
+export const guidePages: GuidePage[] = baseGuidePages.map(page => {
+  return researchedPages.find(researchedPage => researchedPage.id === page.id) ?? page;
+});
 
-export const guidePageMap = Object.fromEntries(guidePages.map(page => [page.id, page]));
+export const guidePageMap = Object.fromEntries(guidePages.map(page => [page.id, page])) as Record<string, GuidePage>;
 
 export { drawerGroups };
