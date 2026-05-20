bdaaea5c45cca4313a6e04e532a49c6df43ee162
Emanuel628 <144055605+Emanuel628@users.noreply.github.com>
2026-05-20 17:08:14 -0400

    Wire Chapter 10 into enhanced guide content

--- app/src/content/pagesEnhanced.ts
+++ app/src/content/pagesEnhanced.ts
@@ -3,8 +3,9 @@ import { drawerGroups, guidePages as baseGuidePages } from './pages';
 import { sixtySeventyRecapPage } from './chapter7';
 import { resetRestartPage } from './chapter8';
 import { sleeperBerthPage } from './chapter9';
+import { hosExceptionsPage } from './chapter10';
 
-const researchedPages: GuidePage[] = [sixtySeventyRecapPage, resetRestartPage, sleeperBerthPage];
+const researchedPages: GuidePage[] = [sixtySeventyRecapPage, resetRestartPage, sleeperBerthPage, hosExceptionsPage];
 
 const phmsaChart16Note = 'Official PHMSA general guidance chart. Verify actual compliance against current 49 CFR hazardous materials regulations.';
 
